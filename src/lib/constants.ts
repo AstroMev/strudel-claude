18087638b728f8fd8fe07673aa156a7276d5a677
AstroMev <35166412+AstroMev@users.noreply.github.com>
2026-05-05 14:34:22 +0900

    デフォルト曲を「歓喜の歌」に変更

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,12 +7,17 @@
  */
 
 /** Default Strudel code shown when the editor first loads */
-export const DEFAULT_CODE = `// Welcome to Strudel!
+export const DEFAULT_CODE = `// 歓喜の歌 - ベートーヴェン 第9交響曲より
 setcpm(120/4)
 
-// Drums
-$: s("bd*4, [~ cp]*2, hh*8").bank("RolandTR909")
+// メロディ
+$: note("[e4@2 e4@2 f4@2 g4@2 g4@2 f4@2 e4@2 d4@2 c4@2 c4@2 d4@2 e4@2 e4@3 d4@1 d4@4 e4@2 e4@2 f4@2 g4@2 g4@2 f4@2 e4@2 d4@2 c4@2 c4@2 d4@2 e4@2 d4@3 c4@1 c4@4]")
+  .s("piano").room(0.3).slow(8)
 
-// Bass
-$: note("<c2 eb2 f2 g2>").s("sawtooth").lpf(400).lpenv(2)
-`
+// ベース
+$: note("<c2 g2 c2 g2 a2 g2 c2 g2>").s("piano").gain(0.6)
+
+// 和音
+$: note("<[c4,e4,g4] [g3,b3,d4] [c4,e4,g4] [g3,b3,d4] [a3,c4,e4] [g3,b3,d4] [c4,e4,g4] [g3,b3,d4]>")
+  .s("piano").gain(0.35)
+`;
